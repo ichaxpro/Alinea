2fa224af7a6b9ddbbc7cdf38903a8a7a6bece83e
= <emrizky804@gmail.com>
2026-06-11 09:29:00 +0700

    chore: renaming app/views and update readme

--- app/View/Components/IconBookmark.php
+++ app/View/Components/IconBookmark.php
@@ -6,7 +6,7 @@ use Closure;
 use Illuminate\Contracts\View\View;
 use Illuminate\View\Component;
 
-class icon-like extends Component
+class IconBookmark extends Component
 {
     /**
      * Create a new component instance.
@@ -21,6 +21,6 @@ class icon-like extends Component
      */
     public function render(): View|Closure|string
     {
-        return view('components.icon-like');
+        return view('components.icon-bookmark');
     }
 }
--- app/View/Components/beranda.php
+++ app/View/Components/beranda.php
@@ -6,7 +6,7 @@ use Closure;
 use Illuminate\Contracts\View\View;
 use Illuminate\View\Component;
 
-class beranda extends Component
+class Beranda extends Component
 {
     /**
      * Create a new component instance.
