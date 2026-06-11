412ac26fd8b279e0b91945fb5f62ba7bcde7e8f9
= <emrizky804@gmail.com>
2026-06-11 09:44:39 +0700

    chore: remove unused line

--- resources/js/detail_buku.js
+++ resources/js/detail_buku.js
@@ -1,12 +1,4 @@
 import "./custom-select";
-
-   // detail_buku.js — Alinea Book Detail Page
-   // Data-driven: semua data dari JS, siap integrasi DB
-   
-   // Untuk integrasi database nanti, cukup ganti BOOK_DATA, REVIEWS,
-   // dan SIMILAR_BOOKS dengan data dari controller via:
-   // window.__BOOK_DATA__ = {!! json_encode($book) !!};
-
 import { renderBookDetail, renderRatingBreakdown } from "./detail_buku/ui";
 import { loadReviews, initReviewsEvents } from "./detail_buku/reviews";
 import { loadSimilarBooks } from "./detail_buku/similar";
